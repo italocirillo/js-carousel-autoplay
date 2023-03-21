97e1cede0215bf9e911e2275ecc26a4cf8e2c40a
italocirillo <italocirillo@gmail.com>
2023-03-21 17:52:25 +0100

    BONUS 2 FATTO

--- js/script.js
+++ js/script.js
@@ -27,8 +27,15 @@ arrayImmagini[indiceAttuale].classList.add("elemento-immagine-lato-attivo");
 const bottonePrecedente=document.querySelector(".precedente");
 const bottoneSuccessivo=document.querySelector(".successiva");
 
+
+
 // TIMEOUT CHE SCORRE L'IMMAGINI IN AUTOMATICO
 let intervalloImmagini = setInterval(immagineSuccessiva, 3000);
+
+// Hover sull'immagine grande che blocca l'autoplay
+contenitoreImmagineGrande.addEventListener("mouseover",function(){
+    clearInterval(intervalloImmagini);
+});
 
 // Bottone Successivo premuto
 bottoneSuccessivo.addEventListener("click", function(){
@@ -42,6 +49,7 @@ bottoneSuccessivo.addEventListener("click", function(){
  bottonePrecedente.addEventListener("click", function(){
     immaginePrecedente();
  });
+
 
  /*******************************************************/
 //  FUNZIONI
